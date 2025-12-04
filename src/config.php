d15ff2263f23819aef01c79cde62f6f82ad8ac1c
mukunzi10 <mukunzigustavus@gmail.com>
2025-12-04 10:47:29 +0200

    Registration and Login functionalities have been Integrated..

--- src/config.php
+++ src/config.php
@@ -5,7 +5,7 @@ function getMongoConnection() {
     try {
         // MongoDB connection string
         $mongoHost = getenv('MONGO_HOST') ?: '24RP14238_mongodb';
-        $mongoPort = getenv('MONGO_PORT') ?: '27017';
+        $mongoPort = getenv('MONGO_PORT') ?: '27019';
         $mongoDb = getenv('MONGO_DB') ?: '24RP14238_shareride_db';
         
         $connectionString = "mongodb://{$mongoHost}:{$mongoPort}";
